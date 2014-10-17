542d6dc9ef512cc73364015f9d2b59cbba44a9ec
Ægir Már Jónsson <aegir13@ru.is>
2014-10-17 15:55:52 +0000

    making the code of CalculatorTest.java prettier

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,27 +8,28 @@ public class CalculatorTest {
         org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-	@Test
-	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
+    @Test
+    public void testEmptyString() {
+	assertEquals(0, Calculator.add(""));
+    }
 
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
+    @Test
+    public void testOneNumber() {
+	assertEquals(1, Calculator.add("1"));
+    }
 
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
+    @Test
+    public void testTwoNumbers() {
+	assertEquals(3, Calculator.add("1,2"));
+    }	
 
-	@Test
- 	public void testMultipleNumbers(){
-    		assertEquals(6, Calculator.add("1,2,3"));
-	}
-	@Test
-	public void testNegativeNumbers(){
-		assertEquals(-5, Calculator.add("-1,-4"));
-	}
+    @Test
+    public void testMultipleNumbers() {
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+    @Test
+    public void testNegativeNumbers() {
+	assertEquals(-5, Calculator.add("-1,-4"));
+    }
 }
